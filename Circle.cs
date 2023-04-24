161df779cec89bda7c0ed8b5a995dce6afb16fcb
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-24 15:26:51 +0700

    Hệ các đối tượng hình học

---
+++ Circle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeCacDoiTuongHinhHoc
+{
+    class Circle : Shape
+    {
+        double radius = 1.0;
+
+        public Circle()
+        {
+
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public Circle(double radius, string color, bool filled) : base(color, filled)
+        {
+            this.radius = radius;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public double getPerimeter()
+        {
+            return 2 * radius * Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return "A Circle with radius="
+                     + getRadius()
+                     + " and with area="
+                     + getArea()
+                     + ", which is a subclass of "
+                     + base.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeCacDoiTuongHinhHoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shape Test
+            Shape shape = new Shape();
+            Console.WriteLine(shape);
+
+            shape = new Shape("red", false);
+            Console.WriteLine(shape);
+
+            // Circle Test
+            Circle circle = new Circle();
+            Console.WriteLine(circle);
+
+            circle = new Circle(3.5);
+            Console.WriteLine(circle);
+
+            circle = new Circle(3.5, "indigo", false);
+            Console.WriteLine(circle);
+
+            // Rectangle Test
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.3, 5.8);
+
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.5, 3.8, "orange", true);
+
+            Console.WriteLine(rectangle);
+
+            // Square Test
+            Square square = new Square();
+            Console.WriteLine(square);
+
+            square = new Square(2.3);
+            Console.WriteLine(square);
+
+            square = new Square(5.8, "yellow", true);
+            Console.WriteLine(square);
+        }
+    }
+}
